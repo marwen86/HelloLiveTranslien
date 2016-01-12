532ae32d6b8a1952015b84383359b54d32d1fd42
majid chaibi <abdelmadjid.chaibi@niji.fr>
2016-01-12 14:53:49 +0100

    Add stations files

--- LiveTrain2.0/trainsViewController.swift
+++ LiveTrain2.0/trainsViewController.swift
@@ -9,16 +9,30 @@
 
 import UIKit
 
-class TrainsViewController: UIViewController , UITextFieldDelegate {
+class TrainsViewController: UIViewController , UITextFieldDelegate, UIPickerViewDelegate, UIPickerViewDataSource {
 
     @IBOutlet weak var startStation: UITextField?
-    @IBOutlet weak var endStation: UITextField?
+    @IBOutlet weak var endStation:   UITextField?
+    @IBOutlet var pickerBizCat: UIPickerView! = UIPickerView()
+    
+    var stationsDict : NSDictionary!
+    var startStationSelected = true
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        pickerBizCat.hidden = true;
+        
         // Do any additional setup after loading the view, typically from a nib.
         let trainLiveDownloader = LiveTrainDownloader()
 
+        if let path = NSBundle.mainBundle().pathForResource("stations", ofType: "plist") {
+            stationsDict = NSDictionary(contentsOfFile: path)
+        }
+        if let _ = stationsDict {
+            print("dict \(stationsDict.allKeys.count)")
+        }
+        
         trainLiveDownloader.downloadListTrain(87276030) { (trains) -> () in
             //list des tarin
             _ = trains
@@ -27,19 +41,57 @@ class TrainsViewController: UIViewController , UITextFieldDelegate {
     }
     
     @IBAction func searchAction(sender: UIButton) {
-        print("search Action");
+        let idStartStation  = stationsDict[startStation!.text!]
+        let idEndStation  = stationsDict[endStation!.text!]
+
+        if let start = idStartStation
+        {
+             if let arrive = idEndStation
+             {
+                print("search Action \(start) \(arrive)");
+            }
+        }
     }
     
-    // Delegate UITextField
-    func textFieldDidBeginEditing(textField: UITextField) {    //delegate method
+
+    // returns the number of 'columns' to display.
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int{
+        return 1
+    }
+    
+    // returns the # of rows in each component..
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int{
+        return stationsDict.allKeys.count
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         
+        return  stationsDict.allKeys[row] as? String
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int)
+    {
+        if(startStationSelected)
+        {
+            startStation?.text = stationsDict.allKeys[row] as? String
+
+        }else{
+            endStation?.text = stationsDict.allKeys[row] as? String
+        }
+        
+        pickerBizCat.hidden = true;
+    }
+    
+    func textFieldShouldBeginEditing(textField: UITextField) -> Bool {
         if (self.startStation == textField)
         {
-            print("click text StartStation TextField")
+            startStationSelected = true
         }else
         {
-            print("click text EndStation TextField")
+            startStationSelected = false
         }
+        pickerBizCat.hidden = false
+        return false
     }
     
     override func didReceiveMemoryWarning() {
